4f2b1ba38dab4e7e6885c1d423180c0964c94221
William Barker <willbarkerwork@gmail.com>
2021-05-06 10:07:15 +0100

    unneeded fragment

--- src/ControlledDropdown/ControlledDropdown.js
+++ src/ControlledDropdown/ControlledDropdown.js
@@ -6,42 +6,37 @@ const ControlledDropdown = ({ label, options, name }) => {
   const { control } = useFormContext();
 
   return (
-    <>
-      <Controller
-        name={name}
-        control={control}
-        render={({
-          field: { onChange, value, name },
-          fieldState: { error },
-        }) => (
-          <TextField
-            select
-            onChange={onChange}
-            defaultValue="none"
-            value={value}
-            name={name}
-            error={!!error}
-            SelectProps={{
-              native: true,
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { onChange, value, name }, fieldState: { error } }) => (
+        <TextField
+          select
+          onChange={onChange}
+          defaultValue="none"
+          value={value}
+          name={name}
+          error={!!error}
+          SelectProps={{
+            native: true,
+            defaultValue: "none",
+            inputProps: {
+              name: name,
               defaultValue: "none",
-              inputProps: {
-                name: name,
-                defaultValue: "none",
-              },
-            }}
-          >
-            <option value="none" disabled>
-              Please Select
+            },
+          }}
+        >
+          <option value="none" disabled>
+            Please Select
+          </option>
+          {options?.map((option) => (
+            <option key={option?.id} value={option?.value}>
+              {option?.label}
             </option>
-            {options?.map((option) => (
-              <option key={option?.id} value={option?.value}>
-                {option?.label}
-              </option>
-            ))}
-          </TextField>
-        )}
-      />
-    </>
+          ))}
+        </TextField>
+      )}
+    />
   );
 };
 
