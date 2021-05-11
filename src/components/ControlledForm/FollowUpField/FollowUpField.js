ce6fff4f47765fa78d11d7f9e15ab3d6dd200144
Jez Williams <jez@jezfx.com>
2021-05-11 09:16:39 +0100

    convert condition to a callback function

--- src/components/ControlledForm/FollowUpField/FollowUpField.js
+++ src/components/ControlledForm/FollowUpField/FollowUpField.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { useFormContext } from "react-hook-form";
-import {
-  getFollowUp,
-  isConditionMet,
-  hasFollowUp,
-} from "./FollowUpField.utils";
+import { getFollowUp, hasFollowUp } from "./FollowUpField.utils";
 
 export const renderFollowUpFields = (fields = []) =>
   fields.map(({ Component, ...props }, index) => (
@@ -24,11 +20,9 @@ export const renderFollowUpFields = (fields = []) =>
 const FollowUpField = ({ fieldName, followUp }) => {
   const { watch } = useFormContext();
   const { condition, fields } = followUp || {};
-
   const fieldValue = watch(fieldName);
-  const showFollowUpFields = isConditionMet(condition, fieldValue);
 
-  return showFollowUpFields ? renderFollowUpFields(fields) : null;
+  return condition(fieldValue) ? renderFollowUpFields(fields) : null;
 };
 
 export default FollowUpField;
